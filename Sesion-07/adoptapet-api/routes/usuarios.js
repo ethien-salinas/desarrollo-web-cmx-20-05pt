d2373f544d0a21a7c70311d8d4f2848c10a355e1
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-09-02 21:16:05 -0500

    [add] single user route

--- Sesion-07/adoptapet-api/routes/usuarios.js
+++ Sesion-07/adoptapet-api/routes/usuarios.js
@@ -9,7 +9,7 @@ const {
 var auth = require('./auth');
 
 router.get('/', auth.requerido, obtenerUsuarios)
-router.get('/:id', auth.requerido, obtenerUsuarios);
+router.get('/:id', auth.requerido, obtenerUsuarios)
 router.post('/', crearUsuario)
 router.post('/entrar', iniciarSesion)
 router.put('/:id', auth.requerido, modificarUsuario)
